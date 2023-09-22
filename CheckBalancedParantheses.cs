1257243d2ad1cf0e19c7b3948f780f8f5ebb711a
Claus Damgaard <jinxxed@outlook.com>
2023-09-22 22:37:10 +0200

    Add project files.

---
+++ CheckBalancedParantheses.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLAP_Week3_Exercise3_2_BalancedParantheses
+{
+    internal static class CheckBalancedParantheses
+    {
+        /// <summary>
+        /// Static method for checking if there's a balanced number of parantheses in a string -
+        /// that is are all opened parantheses closed again, and does the string only contain valid
+        /// parantheses.
+        /// Valid parantheses for this assignent are "[", "(" and "{" and the reversed versions.
+        /// </summary>
+        /// <param name="s">string to check</param>
+        /// <returns>true if balanced, else false</returns>
+        internal static bool Check(string s)
+        {
+            Stack<char> stack = new();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(' || s[i] == '[' || s[i] == '{') stack.Push(s[i]);
+                else if (s[i] == ')')
+                {
+                    if (stack.Count > 0 && stack.Pop() == '(') continue;
+                    else return false;
+                }
+                else if (s[i] == '}')
+                {
+                    if (stack.Count > 0 && stack.Pop() == '{') continue;
+                    else return false;
+                }
+                else if (s[i] == ']')
+                {
+                    if (stack.Count > 0 && stack.Pop() == '[') continue;
+                    else return false;
+                }
+                else return false;
+            }
+            if (stack.Count == 0) return true;
+            return false;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SLAP_Week3_Exercise3_2_BalancedParantheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Checking [{({[]})}] - Should be true");
+            Console.WriteLine(CheckBalancedParantheses.Check("[{({[]})}]"));
+            Console.WriteLine("----------");
+
+            Console.WriteLine("Checking [(()}] - Should be false");
+            Console.WriteLine(CheckBalancedParantheses.Check("[(()}]"));
+            Console.WriteLine("----------");
+
+            Console.WriteLine("Checking (((())) - Should be false");
+            Console.WriteLine(CheckBalancedParantheses.Check("(((()))"));
+            Console.WriteLine("----------");
+
+            Console.WriteLine("Checking {{}}}} - Should be false");
+            Console.WriteLine(CheckBalancedParantheses.Check("{{}}}}"));
+            Console.WriteLine("----------");
+
+            Console.WriteLine("Checking [{(({[[{(({[]}))}]]}))}] - Should be true");
+            Console.WriteLine(CheckBalancedParantheses.Check("[{(({[[{(({[]}))}]]}))}]"));
+            Console.WriteLine("----------");
+
+            Console.WriteLine("Checking ((aa)) - Should be false");
+            Console.WriteLine(CheckBalancedParantheses.Check("((a))"));
+
+        }
+    }
+}
